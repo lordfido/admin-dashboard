a9fcf81e7854d5eaea8f64a9e69de013c446d4ac
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-03-14 16:38:53 +0100

    Layout is not hardcoded in App anymore

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useMemo } from 'react';
 import { Provider, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import GuestLayout from './components/Layout/GuestLayout';
-import MainLayout from './components/Layout/MainLayout';
 import { DASHBOARD, ROOT } from './constants/clientRoutes';
 import { guestRoutes, userRoutes } from './routes';
 import { configureStore } from './store';
@@ -11,37 +9,32 @@ import { isLoggedUser } from './store/selectors';
 const ConnectedApp = (): JSX.Element => {
   const navigate = useNavigate();
   const isLogged = useSelector(isLoggedUser);
-  const { Layout, routes } = useMemo(() => isLogged
-    ? {
-      Layout: MainLayout,
-      routes: userRoutes
-    }
-    : {
-      Layout: GuestLayout,
-      routes: guestRoutes
-    },
+  const routes = useMemo(() => isLogged
+    ? userRoutes
+    : guestRoutes,
   [isLogged]);
 
   useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log('Navigate has been updated');
     if (isLogged) {
       navigate(DASHBOARD);
     } else {
       navigate(ROOT);
     }
-  }, [isLogged, navigate]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLogged]);
   
   return (
-    <Layout>
-      <Routes>
-        { routes.map(route => (
-          <Route
-            key={ route.path }
-            path={ route.path }
-            element={ <route.Component /> }
-          />
-        )) }
-      </Routes>
-    </Layout>
+    <Routes>
+      { routes.map(route => (
+        <Route
+          key={ route.path }
+          path={ route.path }
+          element={ <route.Component /> }
+        />
+      )) }
+    </Routes>
   );
 };
 
