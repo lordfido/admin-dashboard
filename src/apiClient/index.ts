526f6a4d0e750804a0a1ac142d52552def32eaa2
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-03-14 18:19:30 +0100

    apiClient now uses http utility function

--- src/apiClient/index.ts
+++ src/apiClient/index.ts
@@ -1,21 +1,32 @@
 import { LOGIN } from '../constants/serverRoutes';
-import { UserState } from '../store/types';
+import { createRepositoryFromServer } from '../modelCreators';
+import { RepositoriesResponse, Repository, UserState } from '../store/types';
+import http from '../utils/fetch';
 
 const login = async (email: string, password: string): Promise<UserState | undefined> => {
-  const body = {
-    email,
-    password
-  };
-
-  const response = await fetch(LOGIN.path, {
-    method: LOGIN.method,
-    body: JSON.stringify(body)
+  const data = await http<UserState>({
+    ...LOGIN,
+    body: {
+      email,
+      password
+    }
   });
 
-  const data = await response.json();
   return data;
 };
 
-const apiClient = { users: { login } };
+const getData = async (): Promise<Repository[]> => {
+  const data = await http<RepositoriesResponse>({
+    // If this were an API, this URL should be also abstracted
+    path: 'https://api.github.com/search/repositories?sort=stars&q=react'
+  });
+
+  return (data?.items ?? []).map(createRepositoryFromServer);
+};
+
+const apiClient = {
+  users: { login },
+  wheels: { data: getData }
+};
 
 export default apiClient;
