62b3142a28c4cf7bb9f7dbe3d1e9cc37695bfaff
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-03-14 14:30:24 +0100

    Improve GuestLayout component

--- src/components/Layout/GuestLayout/styles.ts
+++ src/components/Layout/GuestLayout/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { flexCentered } from '../../../constants/styles/mixins';
-import { GUEST_BG } from '../../../constants/styles/skin';
+import { GUEST_BG, GUEST_BG_GRADIENT } from '../../../constants/styles/skin';
 
 export const GuestLayoutWrapper = styled.div`
   ${ flexCentered }
   background-color: ${ GUEST_BG };
+  background: ${ GUEST_BG_GRADIENT };
   height: 100%;
 `;
 GuestLayoutWrapper.displayName = 'GuestLayoutWrapper';
