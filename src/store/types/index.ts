e6e4c33d2753ae229ddb266a92e437ee0da3bf5e
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-03-14 18:19:17 +0100

    Improve some types

--- src/store/types/index.ts
+++ src/store/types/index.ts
@@ -1,7 +1,10 @@
+import { RepositoriesState } from './repositories';
 import { UserState } from './user';
 
+export * from './repositories';
 export * from './user';
 
 export interface RootState {
+  repositories: RepositoriesState;
   user: UserState;
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { rest } from 'msw';
+
 export enum RoleId {
   admin = 1,
   user = 2,
@@ -5,3 +7,6 @@ export enum RoleId {
 
 export type RoleNames = Record<RoleId, string>;
 
+export type HttpMethod = Exclude<keyof typeof rest, 'all' | 'head' | 'options'>;
+
+export type LoadingState<T = 'loading'> = 'idle' | T | 'success' | 'error';
