5c2753f884b4bd17b03e42ada6ae499780768cd8
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-03-14 14:50:58 +0100

    Add Login container's logic

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../components/Button';
+import Form from '../../components/Forms/Form';
+import Input from '../../components/Forms/Input';
+import Text from '../../components/Text';
+import { LoginWrapper } from './styles';
 
-const Login = (): JSX.Element => (
-  <div className="Login">
-    Login
-  </div>
-);
+const Login = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = (newValue: string) => {
+    setEmail(newValue);
+  };
+
+  const onChangePassword = (newValue: string) => {
+    setPassword(newValue);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    // eslint-disable-next-line no-console
+    console.log({ email, password });
+  };
+
+  return (
+    <LoginWrapper className="Login">
+      <Text type="title1">Sign In</Text>
+
+      <Form onSubmit={ onSubmit }>
+        <Input
+          defaultValue={ email }
+          id="LoginForm-email"
+          onChange={ onChangeEmail }
+        >
+          Email
+        </Input>
+
+        <Input
+          defaultValue={ password }
+          id="LoginForm-password"
+          onChange={ onChangePassword }
+          type="password"
+        >
+          Password
+        </Input>
+
+        <Button type="submit">
+          Sign In
+        </Button>
+      </Form>
+    </LoginWrapper>
+  );
+};
 
 export default Login;
---
+++ src/containers/Login/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import Card from '../../components/Card';
+
+export const LoginWrapper = styled(Card)`
+  min-width: 300px;
+  padding: 30px 20px;
+  width: 50%;
+`;
+LoginWrapper.displayName = 'LoginWrapper';
