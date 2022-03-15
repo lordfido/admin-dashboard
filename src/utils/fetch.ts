b8b5d67175a2217d6bf5879d14517efa2862c64f
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2022-03-15 17:34:18 +0100

    Allow fetch utility to use authorization headers

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,3 +1,4 @@
+import { API_BASE, API_PORT } from '../constants/serverRoutes';
 import { HttpMethod } from '../types';
 
 interface Props {
@@ -6,12 +7,33 @@ interface Props {
   body?: unknown;
 }
 
+export const customApiUrl = (path: string) => `${ API_BASE }:${ API_PORT }${ path }`;
+
+let authorization: string;
+
+const getAuthToken = () => authorization ? `Bearer ${ authorization }` : undefined;
+
+export const setAuthToken = (token: string) => {
+  authorization = token;
+};
+
+
 const http = async <T>({
   body,
   method = 'get',
   path
 }: Props): Promise<T> => {
+  const headers = new Headers();
+  
+  headers.set('Content-Type', 'application/json');
+  
+  const authToken = getAuthToken();
+  if (authToken) {
+    headers.set('Authorization', authToken);
+  }
+
   const response = await fetch(path, {
+    headers,
     method: method,
     body: JSON.stringify(body)
   });
